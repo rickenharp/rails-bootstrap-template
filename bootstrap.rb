ed90c6af4f06adb6179fe57b232b8d305a41dde5
Alexander Graefe <alex@codemo.de>
2012-07-01 16:02:46 +0200

    Add rspec and cucumber

--- bootstrap.rb
+++ bootstrap.rb
@@ -6,3 +6,25 @@ rake("db:migrate")
 git :init
 git :add => "."
 git :commit => "-a -m 'Initial commit'"
+
+# rspec
+
+gem_group :development, :test do
+  gem "rspec-rails"
+end
+run "bundle install"
+generate('rspec:install')
+git :add => "."
+git :commit => "-a -m 'Add rspec'"
+
+# cucumber
+
+gem_group :test do
+  gem 'cucumber-rails', :require => false
+  gem 'database_cleaner'
+end
+run "bundle install"
+generate('cucumber:install')
+git :add => "."
+git :commit => "-a -m 'Add cucumber'"
+
